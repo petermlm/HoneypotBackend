7982265681b5bd24e245cf3e3814a9694f46d780
Pedro Melgueira <pedromelgueira@gmail.com>
2022-04-23 16:15:58 +0200

    Moves send_some_text.sh to scripts

--- scripts/send_some_text.sh
+++ scripts/send_some_text.sh
@@ -3,4 +3,4 @@
 # This script, or a variant thereof, should only be used for experiments in
 # development purposes.
 
-echo -ne "This is some random text" | netcat localhost 5432
+echo -ne "This is some text" | netcat localhost 5432
